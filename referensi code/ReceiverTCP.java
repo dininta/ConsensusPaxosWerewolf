fd685713fdd93f694bec77d9a0fc4efd9d4931a3
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-04-29 00:15:22 +0700

    Added TCP

---
+++ referensi code/ReceiverTCP.java
@@ -0,0 +1,66 @@
+//import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.net.InetAddress;
+
+public class ReceiverTCP {
+	
+	protected static String dstAddress;
+	protected static int dstPort;
+	protected static InetAddress IPAddress;
+	protected static String request;
+	protected static String response;
+
+
+	public static void main(String args[]) throws Exception
+	{
+		Socket socket = null;
+		BufferedReader in;
+		dstAddress = "localhost";
+		IPAddress = InetAddress.getByName(dstAddress);
+		dstPort = 9876;
+		//JSONObject json;
+		try {
+            socket = new Socket(dstAddress, dstPort);
+            System.out.println("Connecting...");
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            int c;
+            response = "";
+            while((c=in.read())!=-1){
+                response+=(char)c;
+            }
+            //json = new JSONObject();
+            //json.put("method", "join");
+            //json.put("username", "cantik");
+            System.out.println("Response: " + response);
+            in.close();
+            socket.close();
+        //} catch(org.json.JSONException e){
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            response = "UnknownHostException: " + e.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = "IOException: " + e.toString();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+	}
+}
---
+++ referensi code/SenderTCPBaru.java
@@ -0,0 +1,86 @@
+import org.json.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.net.InetAddress;
+
+public class SenderTCP {
+	
+	protected String dstAddress;
+	protected int dstPort;
+	protected InetAddress IPAddress;
+	protected ServerSocket serverSocket = null;
+	protected Socket socket = null;
+	protected PrintWriter out;
+	protected JSONObject json;
+	protected String username;
+
+	public SenderTCP(){
+		try {
+			dstAddress = "localhost";
+			IPAddress = InetAddress.getByName(dstAddress);
+			dstPort = 9876;
+			serverSocket = new ServerSocket(dstPort);
+			System.out.println("Connecting...");
+            socket = serverSocket.accept();
+            System.out.println("Server has connected");
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);            
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                disconnect();
+            }
+        }
+	}
+
+	public void joinGame(){
+		// Get username
+		BufferedReader inFromuser = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Username: ");
+		try {
+			username = inFromuser.readLine();
+		} catch (IOException e) {}
+
+		// Create json
+		try{
+			json = new JSONObject();
+	        json.put("method", "join");
+	        json.put("username", username);
+        } catch (org.json.JSONException e) {}
+
+        // Send json
+	    System.out.println("Request: " + json.toString());
+	    out.println(json.toString());
+	}
+
+	public void disconnect(){
+		try {
+            out.close();
+	        socket.close();
+	        serverSocket.close();
+        } catch (IOException e) {e.printStackTrace();}
+		
+	}
+
+	public static void main(String args[]) throws Exception
+	{
+		SenderTCP senderTCP = new SenderTCP();
+		//senderTCP.setup();
+		senderTCP.joinGame();
+		senderTCP.disconnect();
+		
+	}
+}
